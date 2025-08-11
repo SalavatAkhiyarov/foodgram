d5e6c7f9c8792a46c3d53fe92f31ce47885ffa68
Salavat Akhiyarov <salavatahiyarov@icloud.com>
2025-08-11 21:34:21 +0300

    Тест2

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,9 +1,8 @@
+from api.views import short_link_redirect
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
-from api.views import short_link_redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
